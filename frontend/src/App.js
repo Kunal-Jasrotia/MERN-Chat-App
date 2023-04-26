3d733c6cc008c38b59fc087565a2609dcf1e1bce
kunal-6421 <123140152+kunal-6421@users.noreply.github.com>
2023-04-26 14:46:21 +0530

    added mongo db models

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,12 @@ import Homepage from './pages/Homepage';
 import Chats from './pages/Chats';
 function App() {
   return (
-    <>
+    <div className='App'>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/chats' element={<Chats />} />
       </Routes >
-    </>
+    </div>
   );
 }
 
